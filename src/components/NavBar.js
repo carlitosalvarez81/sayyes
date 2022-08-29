d7eb9524e8d9d84ed3877f4bed5fa6ea9e4a7252
Carlos <carlos.jalvarez@hotmail.com>
2022-08-29 18:10:23 -0300

    componente NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <header>
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default NavBar;
