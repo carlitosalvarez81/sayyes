2834df4e634212e659cb8157b660571a19a62b88
Carlos <carlos.jalvarez@hotmail.com>
2022-09-14 18:56:23 -0300

    primera entrega y desafio pendiente

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,37 +1,38 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/esm/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import { useState } from "react";
 
+function ItemCount({ stock, initial }) {
 
-function ItemCount({ stock, initial, onAdd }) {
-    
-    return (
-        <div>
-            <Container>
-                <Row>
-                    <Col xl={2}>
-                        <InputGroup className="mb-3">
-                            <Button variant="outline-secondary">
-                                -
-                            </Button>
-                            <Form.Control
-                                aria-label="Example text with button addon"
-                                aria-describedby="basic-addon1"
-                                value={initial}
-                            />
-                            <Button variant="outline-secondary" onClick={onAdd}>
-                                +
-                            </Button>
-                        </InputGroup>
-                        <Button variant="primary">Agregar al carrito</Button>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+  const [count, setCount] = useState(initial);
+
+  return (
+    <div>
+      <div className="d-flex align-items-center justify-content-between">
+        <InputGroup>
+          <Button variant="outline-primary">-</Button>
+          <Form.Control
+            aria-label="Example text with button addon"
+            aria-describedby="basic-addon1"
+            value={count}
+          />
+          <Button variant="outline-primary" onClick={
+            () => {
+              if( count + 1 <= stock){
+                setCount(count + 1)
+              }
+            }
+          }>
+            +
+          </Button>
+        </InputGroup>
+        <Button className="ms-3" variant="outline-primary">
+          Agregar
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default ItemCount;
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,73 +1,84 @@
-import React, { useEffect } from 'react'
-import Container from 'react-bootstrap/esm/Container';
-import Item from './Item';
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Item from "./Item";
+import { useEffect, useState } from "react";
+import Spinner from 'react-bootstrap/Spinner';
 
-export default function ItemList(card) {
 
-    const vestidos = [
-        {
-            "nombre" : "Vainillla",
-            "descripcion" : "Usado",
-            "precio": 50000
-        }, 
-        {
-            "nombre" : "Campo",
-            "descripcion" : "Nuevo",
-            "precio": 100000
-        }, 
-        {
-            "nombre" : "Clásico",
-            "descripcion" : "Nuevo",
-            "precio": 150000
-        }, 
-    ]
+export default function ItemList() {
+  const vestidosArr = [
+    {
+      id: 1,
+      nombre: "Vainillla",
+      descripcion: "Usado",
+      precio: 50000,
+      stock: 5,
+    },
+    {
+      id: 2,
+      nombre: "Campo",
+      descripcion: "Nuevo",
+      precio: 100000,
+      stock: 3,
+    },
+    {
+      id: 3,
+      nombre: "Clásico",
+      descripcion: "Nuevo",
+      precio: 150000,
+      stock: 10,
+    },
+    {
+      id: 4,
+      nombre: "Moderno",
+      descripcion: "Nuevo",
+      precio: 150000,
+      stock: 2,
+    },
+  ];
 
-    useEffect(() => {
-      console.log("primero")
-        items.then(res => {
-            console.log(res)
-        }, err => {
-            console.log(err)
-        }).catch(err => {
-            console.log(err)
-        })
-      return () => {
-        console.log("Segundo")
-      }
-    }, [])
-    
-    
+  const [vestidos, setVestidos] = useState([]);
 
-    const items = new Promise( (resolve, reject) => {
-        
-        setTimeout(() => {
-            const vestidos = [
-                {
-                    "nombre" : "Vainillla",
-                    "descripcion" : "Usado",
-                    "precio": 50000
-                }, 
-                {
-                    "nombre" : "Campo",
-                    "descripcion" : "Nuevo",
-                    "precio": 100000
-                }, 
-                {
-                    "nombre" : "Clásico",
-                    "descripcion" : "Nuevo",
-                    "precio": 150000
-                }, 
-            ]
-        }, 3000);
-        return vestidos;
-    })
-    
+  useEffect(() => {
+    const getItem = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(vestidosArr);
+      }, 2000);
+    });
+
+    getItem.then((res) => {
+      console.log(res);
+      setVestidos(res);
+    });
+  }, []);
 
   return (
     <Container className="mt-5">
-        {vestidos.map(item => {
-            return <Item key={item.nombre} name={item.nombre} description={item.descripcion} price={item.precio}></Item>
-        })}
+      <Row>
+        {vestidos.length !== 0 ? (
+          vestidos.map((item) => {
+            return (
+              <Col>
+                <Item
+                  key={item.id}
+                  name={item.nombre}
+                  description={item.descripcion}
+                  price={item.precio}
+                  id={item.id}
+                  stock={item.stock}
+                ></Item>
+              </Col>
+            );
+          })
+        ) : (
+          <div class="text-center">
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+          </div>
+        )}
+      </Row>
     </Container>
-  )
+  );
 }
