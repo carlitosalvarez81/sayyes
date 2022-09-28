04c1c801e5f8ce2dd7635933879b6c1dda9054f3
Carlos <carlos.jalvarez@hotmail.com>
2022-09-28 18:04:58 -0300

    instalación firebase pero con error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAHP26daqt6_KcOWmVSoR_u8DRwhhWA6-k",
+  authDomain: "sayyes-43bdd.firebaseapp.com",
+  projectId: "sayyes-43bdd",
+  storageBucket: "sayyes-43bdd.appspot.com",
+  messagingSenderId: "943731725243",
+  appId: "1:943731725243:web:dc1f9a8a2a6015a6682a9e",
+  measurementId: "G-4BGQ5G8EDE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
